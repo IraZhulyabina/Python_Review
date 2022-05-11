69b771e7d5edb2f249988aa47bd674e49a5450ae
IraZhulyabina <Zhuliabina.ii@phystech.edu>
2022-05-11 22:06:46 +0300

    added project

---
+++ Website/wall.py
@@ -0,0 +1,12 @@
+class Wall:
+    def __init__(self):
+        self._does_exist = True
+
+    def does_exist(self):
+        return self._does_exist
+
+    def make_nonexistent(self):
+        self._does_exist = False
+
+    def declare_existence(self):
+        self._does_exist = True
